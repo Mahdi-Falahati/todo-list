978b76fff24a6c39a50ff465ecca95e305ea347e
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-03-12 12:52:13 -0700

    add delete function for deleting todo by ID use  delete mthod

--- Server/controllers/ToDoController.js
+++ Server/controllers/ToDoController.js
@@ -12,20 +12,35 @@ module.exports.getToDo = async (req, res) => {
 module.exports.saveToDo = async (req, res) => {
   try {
     const todo = await ToDoModel.create(req.body);
-    res.status(201).json( todo );
+    res.status(201).json(todo);
   } catch (error) {
     res.status(500).json({ msg: error });
   }
 };
 
 module.exports.updateTodo = async (req, res) => {
-  const {id: _id} = req.params
-  const todo = req.body
-  
+  const { id: _id } = req.params;
+  const todo = req.body;
+
   try {
-      const updatedTodo = await ToDoModel.findByIdAndUpdate(_id, {...todo, _id}, {new: true})
-      res.status(200).json(updatedTodo)
+    const updatedTodo = await ToDoModel.findByIdAndUpdate(
+      _id,
+      { ...todo, _id },
+      { new: true }
+    );
+    res.status(200).json(updatedTodo);
   } catch (error) {
-      res.status(500).json({ msg: error });
+    res.status(500).json({ msg: error });
   }
-}
+};
+
+module.exports.deleteTodo = async (req, res) => {
+  const { id: todoID } = req.params;
+  try {
+    const todo = await ToDoModel.findOneAndDelete({ _id: todoID });
+
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
--- Server/routes/ToDoRoute.js
+++ Server/routes/ToDoRoute.js
@@ -3,11 +3,13 @@ const {
   getToDo,
   saveToDo,
   updateTodo,
+  deleteTodo,
 } = require("../controllers/ToDoController");
 
 const router = Router();
 router.get("/", getToDo);
 router.post("/save", saveToDo);
 router.patch("/:id", updateTodo);
+router.delete("/:id", deleteTodo);
 
 module.exports = router;
