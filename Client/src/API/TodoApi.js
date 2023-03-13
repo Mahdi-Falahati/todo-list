c2f5fd33df3738bf29a684f7cab6e295070c6026
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-03-13 11:13:19 -0700

    fixed export functions in api file

--- Client/src/API/TodoApi.js
+++ Client/src/API/TodoApi.js
@@ -2,37 +2,37 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:5000/";
 
-export const getAllTodo = async () => {
+export async function getAllTodo() {
   try {
     const todo = axios.get(baseUrl);
     return todo;
   } catch (error) {
     console.log(error);
   }
-};
+}
 
-export const updateTodo = async (id, todo) => {
+export async function updateTodo(id, todo) {
   try {
     console.log(baseUrl + id);
     await axios.patch(baseUrl + id, todo);
   } catch (error) {
     console.log(error);
   }
-};
+}
 
-export const createTodo = async (todo) => {
+export async function createTodo(todo) {
   try {
-    await axios.post(baseUrl+"save", todo);
+    await axios.post(baseUrl + "save", todo);
   } catch (error) {
     console.log(error);
   }
-};
+}
 
-export const deleteTodo = async (id) => {
+export async function deleteTodo(id) {
   try {
     console.log(baseUrl + id);
     await axios.delete(baseUrl + id);
   } catch (error) {
     console.log(error);
   }
-};
+}
