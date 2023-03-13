5e9a2355be1c4ab372aff0ebef775523fb05cc40
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-03-13 10:39:49 -0700

    add styles to output component

--- Client/src/Components/Output.jsx
+++ Client/src/Components/Output.jsx
@@ -1,9 +1,11 @@
 import Todo from "./Todo";
 import { v4 as uuidv4 } from "uuid";
 
+import "./Output.css"
+
 export default function Output({ todoes }) {
   return (
-    <section>
+    <section className="output">
       {todoes?.map((data) => (
         <Todo data={data} key={uuidv4()} />
       ))}
