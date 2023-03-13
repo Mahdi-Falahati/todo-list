ad850ba9ffa45e66b4deb3a8c8e9074261096beb
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-03-12 23:42:19 -0700

    deleted some backend file from main branch

--- Server/routes/ToDoRoute.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const { getToDo, saveToDo } = require('../controllers/ToDoController');
-
-const router = Router();
-router.get("/",getToDo)
-router.post("/save",saveToDo)
-
-module.exports = router;
