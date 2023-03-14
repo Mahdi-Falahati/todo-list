b147afad4ef7ded577da70a4641fded08ad4f1aa
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-03-14 03:51:21 -0700

    add edit toggle to context for open and close modal

--- Client/src/StoreContext.js
+++ Client/src/StoreContext.js
@@ -3,11 +3,20 @@ export const StoreContext = createContext();
 
 export function StoreContextProvider({ children }) {
   const [store, setStore] = useState(false);
+  const [modal, setModal] = useState(false);
+  
   const storeSetterToggle = (validate) => {
     setStore(validate);
   };
+
+  const setModalToggle = () => {
+    setModal(!modal);
+  };
+  
   return (
-    <StoreContext.Provider value={{ store, storeSetterToggle }}>
+    <StoreContext.Provider
+      value={{ store, modal, setModalToggle, storeSetterToggle }}
+    >
       {children}
     </StoreContext.Provider>
   );
