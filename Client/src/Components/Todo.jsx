0e4120622367d0e33161eb4d70598db7aafb7521
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-03-14 00:59:12 -0700

    remove todo from db

--- Client/src/Components/Todo.jsx
+++ Client/src/Components/Todo.jsx
@@ -1,12 +1,18 @@
+import { deleteTodo } from "../API/TodoApi";
 import "./Todo.css";
 
 export default function Todo({ data,id }) {
+  const deleteTodoHandler =(e)=>{
+    const todo = e.target.parentElement.parentElement
+    deleteTodo(todo.id)
+  }
+
   return (
     <section className="todo" id={id}>
       <span>{data}</span>
       <div className="btns">
         <button className="btn-edit">Edit</button>
-        <button className="btn-delete">X</button>
+        <button className="btn-delete" onClick={deleteTodoHandler}>X</button>
       </div>
     </section>
   );
